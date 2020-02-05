743eb229211d00cdf369f9230eb5ca44ec9bb872
Nijitha <nijithakapudasi@gmail.com>
2020-02-05 11:14:40 +0530

    Final code

---
+++ YOLOedit/yolo_edit.py
@@ -0,0 +1,171 @@
+
+# import the necessary packages
+import numpy as np
+import argparse
+import imutils
+import time as t
+import cv2
+import os
+
+
+# construct the argument parse and parse the arguments
+'''ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", required=True,
+    help="path to input video")
+ap.add_argument("-o", "--output", required=True,
+    help="path to output video")
+ap.add_argument("-y", "--yolo", required=True,
+    help="base path to YOLO directory")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+    help="minimum probability to filter weak detections")
+ap.add_argument("-t", "--threshold", type=float, default=0.3,
+    help="threshold when applyong non-maxima suppression")
+args = vars(ap.parse_args())'''
+
+# load the COCO class labels our YOLO model was trained on
+confidence = 0.5
+threshold = 0.3
+labelsPath = "/home/nijitha/PycharmProjects/opencv/YOLOv3/yolo-coco/coco.names"
+output = "/home/nijitha/PycharmProjects/opencv/YOLOv3/output"
+LABELS = open(labelsPath).read().strip().split("\n")
+
+# initialize a list of colors to represent each possible class label
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+                           dtype="uint8")
+
+# derive the paths to the YOLO weights and model configuration
+weightsPath = "/home/nijitha/PycharmProjects/opencv/YOLOv3/yolo-coco/yolov3.weights"
+configPath = "/home/nijitha/PycharmProjects/opencv/YOLOv3/yolo-coco/yolov3.cfg"
+
+# load our YOLO object detector trained on COCO dataset (80 classes)
+# and determine only the *output* layer names that we need from YOLO
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+# initialize the video stream, pointer to output video file, and
+# frame dimensions
+vs = cv2.VideoCapture("/home/nijitha/PycharmProjects/opencv/YOLOv3/videos/zebg.mp4")
+writer = None
+(W, H) = (None, None)
+# try to determine the total number of frames in the video file
+try:
+    prop = cv2.cv.CV_CAP_PROP_FRAME_COUNT if imutils.is_cv2() \
+        else cv2.CAP_PROP_FRAME_COUNT
+    total = int(vs.get(prop))
+    print("[INFO] {} total frames in video".format(total))
+
+# an error occurred while trying to determine the total
+# number of frames in the video file
+except:
+    print("[INFO] could not determine # of frames in video")
+    print("[INFO] no approx. completion time can be provided")
+    total = -1
+# loop over frames from the video file stream
+while True:
+    # read the next frame from the file
+    (grabbed, frame) = vs.read()
+    # if the frame was not grabbed, then we have reached the end
+    # of the stream
+    if not grabbed:
+        break
+
+    # if the frame dimensions are empty, grab them
+    if W is None or H is None:
+        (H, W) = frame.shape[:2]
+
+    # construct a blob from the input frame and then perform a forward
+    # pass of the YOLO object detector, giving us our bounding boxes
+    # and associated probabilities
+    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
+                                 swapRB=True, crop=False)
+    net.setInput(blob)
+    start = t.time()
+    layerOutputs = net.forward(ln)
+    end = t.time()
+
+    # initialize our lists of detected bounding boxes, confidences,
+    # and class IDs, respectively
+    boxes = []
+    confidences = []
+    classIDs = []
+    total = {}
+    # loop over each of the layer outputs
+    for output in layerOutputs:
+        # loop over each of the detections
+        for detection in output:
+            # extract the class ID and confidence (i.e., probability)
+            # of the current object detection
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence1 = scores[classID]
+            if confidence1 > confidence and 24 >= classID >= 15:
+                id=classID
+                animal = (LABELS[id])
+                #for animal in total.keys():
+                if animal not in total:
+                        total[animal] =0
+                else:
+                        total[animal]+=1
+                # scale the bounding box coordinates back relative to
+                # the size of the image, keeping in mind that YOLO
+                # actually returns the center (x, y)-coordinates of
+                # the bounding box followed by the boxes' width and
+                # height
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+
+                # use the center (x, y)-coordinates to derive the top
+                # and and left corner of the bounding box
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+
+                # update our list of bounding box coordinates,
+                # confidences, and class IDs
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence1))
+                classIDs.append(classID)
+    #print("frame_no, total", frame_no, total)
+
+    # apply non-maxima suppression to suppress weak, overlapping
+    # bounding boxes
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, confidence, threshold)
+
+    # ensure at least one detection exists
+    if len(idxs) > 0:
+        # loop over the indexes we are keeping
+        for i in idxs.flatten():
+            # extract the bounding box coordinates
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+
+            # draw a bounding box rectangle and label on the frame
+            color = [int(c) for c in COLORS[classIDs[i]]]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            text = "{}: {:.4f}".format(LABELS[classIDs[i]],
+                                       confidences[i])
+            cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+            frame_no = vs.get(cv2.CAP_PROP_POS_FRAMES)
+            time = vs.get(cv2.CAP_PROP_POS_MSEC)
+            time = round(time / 1000 , 2)
+            '''dict[frame_no] = {}
+            dict[frame_no]["Animals found"] = no_of_animals
+            dict[frame_no]["Animal"] = {}
+            for index in range(no_of_animals):
+                #dict[frame_no][no_of_animals]["Animal details"] = {}
+                dict[frame_no]["Animal"]["Animal", index+1, "Name"] = total[animal]
+                dict[frame_no]["Time"] = time'''
+            Dict = {}
+            Dict = dict({"animals_list": total, "animals found": len(idxs), " At Frame_NO": frame_no, "Time": time})
+    cv2.imshow('frame', frame)
+    print(Dict)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+# release the file pointers
+print("[INFO] cleaning up...")
+#writer.release()
+vs.release()
